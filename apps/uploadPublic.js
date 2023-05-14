82474e1c35cffe89e122af3e19fe9bbdff3a4aa3
DingAllen <1121174884@qq.com>
2023-05-14 15:39:07 +0800

    初步完成APP的编写，暂未测试，并且下载的部分需要和链码一起优化

---
+++ apps/uploadPublic.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const { Gateway, Wallets } = require('fabric-network');
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline-sync');
+
+async function main() {
+    try {
+        // load the network configuration
+        const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+        let ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const identity = await wallet.get('appUser');
+        if (!identity) {
+            console.log('An identity for the user "appUser" does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('ipfsSaveShare');
+
+        // 提示用户输入文件路径，并读取文件内容
+        var filename = readline.question("请输入要上传的文件路径：");
+        var fp = fs.readFileSync(filename);
+
+        let ne = false;
+        let password = '';
+        var ifEncrypt = readline.question("是否加密上传？(y/n)");
+        if (ifEncrypt == 'y') {
+            ne = true;
+            password = readline.question("请输入加密密码：");
+        }
+
+        // 提交交易
+        console.log('Submitting transaction to upload file...');
+        const result = await contract.submitTransaction('UploadFile', fp, ne, password);
+
+        console.log('文件上传成功！信息：' + result.toString('utf8'));
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        process.exit(1);
+    }
+}
+
+main();
